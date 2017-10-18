2c87eabbd61f6891910f74dc1e2df6057323ef3c
chybeta <chenhengyi@stu.xmu.edu.cn>
2017-10-18 13:54:53 +0800

    [*] update

--- cmspoc.py
+++ cmspoc.py
@@ -1,17 +1,17 @@
 from lib.core.data import target,path
 from lib.parse.cmdline import cmdLineParser
 from lib.core.common import banner
-from lib.controllor.controllor import loadScripts
-from lib.controllor.controllor import run
-from lib.controllor.controllor import init
+from lib.controllor.task import loadScripts
+from lib.controllor.task import runPoc,autoPoc
+# from lib.controllor.init import initPath,
+from lib.controllor.task import start
 import requests
 
-def main():	
+def cli():
 	try:
+		banner()
 		target.update(cmdLineParser().__dict__)
-		banner()		
-		init()
-		run(loadScripts())
+		start()
 	except requests.exceptions.InvalidSchema as e:
 		print("\033[31m[!] Please input the right url.\033[0m\n")
 	except requests.exceptions.MissingSchema as e:
@@ -23,4 +23,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    cli()
