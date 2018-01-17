447e0074e740374b20bdbc9b09a9054c7ca0647b
chybeta <chenhengyi@stu.xmu.edu.cn>
2018-01-17 08:57:50 +0800

    update SITE

--- lib/core/setting.py
+++ lib/core/setting.py
@@ -3,13 +3,13 @@ TYPE = "dev" if VERSION.count('.') > 2 and VERSION.split('.')[-1] != '0' else "s
 TYPE_COLORS = {"dev": 35, "stable": 90, "pip": 34}
 VERSION_STRING = "chybeta/%s#%s" % ('.'.join(VERSION.split('.')[:-1]) if VERSION.count('.') > 2 and VERSION.split('.')[-1] == '0' else VERSION, TYPE)
 SCRIPT_PATH = ""
-SITE = "https://chybeta.github.io"
+SITE = "https://github.com/chybeta/cmspoc"
 
 BANNER = """\033[01;31m\
-                                 ____              
+                                 ____
   _____ ____ ___   _____        / __ \ ____   _____\033[01;37m{\033[01;%dm%s\033[01;37m}\033[01;31m
  / ___// __ `__ \ / ___/______ / /_/ // __ \ / ___/
-/ /__ / / / / / /(__  )/_____// ____// /_/ // /__  
-\___//_/ /_/ /_//____/       /_/     \____/ \___/  
-                                                   \033[0m\033[4;37m%s\033[0m\n 
+/ /__ / / / / / /(__  )/_____// ____// /_/ // /__
+\___//_/ /_/ /_//____/       /_/     \____/ \___/
+                                                   \033[0m\033[4;37m%s\033[0m\n
 """ % (TYPE_COLORS.get(TYPE, 31), VERSION_STRING.split('/')[-1], SITE)
